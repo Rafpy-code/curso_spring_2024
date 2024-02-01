5a5bf832bdebdfbb53fabc48ef30e5e270caf2b8
tarde <tarde@A13E11>
2024-02-01 20:54:34 +0100

    12 funcionando las relaciones join, 13 adjuntar las instrucciones y completar para el lunes lo solicitado.

---
+++ 13_libreria_spring_ventas/src/main/java/model/Venta.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ventas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Venta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idVenta;
+	
+	private Cliente cliente;
+	private Libro libro;
+	private Date fecha;
+	
+}
